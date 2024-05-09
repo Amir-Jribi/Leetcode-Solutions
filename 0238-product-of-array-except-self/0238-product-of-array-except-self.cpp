adc11de62a27ffbf36e586e84825d61b5358d9c4
Amir Jribi <153393132+Amir-Jribi@users.noreply.github.com>
2024-05-09 13:59:16 +0100

    Time: 27 ms (18.55%), Space: 38.1 MB (23.32%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,6 +4,12 @@ public:
         int n=nums.size();
         vector<int>ans(n,1);
         int left=1,right=1;
+//         reduce memory complexity 
+//         instead of using 2 vectors to store the prefix and the suffix
+//         we use just our array ans
+//         and make 2 loops 
+//         one for the left part 
+//         and one for the right part
         for(int i=0;i<n;i++){
             ans[i]*=left;
             left*=nums[i];
