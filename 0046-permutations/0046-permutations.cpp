e3b625e74fa29ae3dde4371527d93aba294a3e68
Amir Jribi <153393132+Amir-Jribi@users.noreply.github.com>
2024-06-18 14:04:04 +0100

    Time: 0 ms (100.00%), Space: 8.7 MB (89.38%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        ans.push_back(nums);
+        while(next_permutation(nums.begin(),nums.end())){
+            ans.push_back(nums);
+        }
+        
+        //  do {
+        //      ans.push_back(nums);
+        //  } 
+        // while (next_permutation(nums.begin(),nums.end()));
+        
+        return ans;
+    }
+};
